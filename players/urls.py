99f293c59d80371b95a6850419db794ab626b895
James Helms <jhelms1014@gmail.com>
2026-06-06 22:19:42 -0500

    Remove debug endpoint

--- players/urls.py
+++ players/urls.py
@@ -7,7 +7,6 @@ from .views import (
     request_password_reset,
     confirm_password_reset,
     forgot_username,
-    debug_users,
 )
 
 # NOTE: A router auto-generates all the URLs for a ViewSet. Because we used
@@ -24,5 +23,4 @@ urlpatterns = router.urls + [
     path('password-reset/', request_password_reset),
     path('password-reset-confirm/', confirm_password_reset),
     path('forgot-username/', forgot_username),
-    path('debug-users/', debug_users),  # TEMPORARY — delete after debugging
 ]
